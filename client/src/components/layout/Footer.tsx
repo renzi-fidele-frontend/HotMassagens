f74f290e0a1e4e13477eede7061d97e68eec0799
Renzi Fidele <renzifidele2001@gmail.com>
2025-05-19 16:34:21 +0200

    feat: Finalizando a responsividade do componente Rodapé

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -8,18 +8,18 @@ const Footer = () => {
    return (
       <footer className="bg-black pt-7 pb-12 text-gray-200">
          <Container>
-            <div className="flex justify-between">
+            <div className="flex justify-between flex-wrap lg:flex-nowrap md:px-6 lg:px-0">
                {/* Identidade */}
-               <div className="basis-[25%]">
+               <div className="basis-[100%] lg:basis-[25%] flex flex-col items-center lg:items-start">
                   <Image className="w-[210px]" src={logo} alt="Logo da Hot Massagens" />
-                  <p className="mt-5 text-gray-300">
+                  <p className="mt-5 mb-6 lg:mb-0 text-gray-300 text-center lg:text-start">
                      Aqui você encontra as melhores massagistas, terapeutas e SPA’s de topo no distrito de Lisboa.
                   </p>
                </div>
                {/* Mapa do site */}
-               <div>
+               <div className="basis-[100%] lg:basis-[auto] flex flex-col items-center lg:items-start">
                   <h6 className="font-heading font-semibold">Mapa do site</h6>
-                  <div className="flex flex-col mt-4 gap-3">
+                  <div className="flex text-lg sm:text-xl lg:text-base lg:flex-col mt-4 mb-7 lg:mb-0 gap-5 lg:gap-3 underline lg:no-underline flex-wrap sm:flex-nowrap justify-center">
                      <Link href="">Início</Link>
                      <Link href="">Sobre</Link>
                      <Link href="">Terapeutas</Link>
@@ -27,7 +27,7 @@ const Footer = () => {
                   </div>
                </div>
                {/* Informações de contato */}
-               <div>
+               <div className="text-center lg:text-start basis-[100%] lg:basis-[auto]">
                   <h6 className="font-heading font-semibold">Informações de contato</h6>
                   <div className="flex flex-col mt-4 gap-3">
                      <Link target="_blank" href="tel:+351910872391">
@@ -42,9 +42,9 @@ const Footer = () => {
                   </div>
                </div>
                {/* Redes sociais */}
-               <div>
+               <div className="text-center lg:text-start basis-[100%] lg:basis-[auto] mt-5 lg:mt-0">
                   <h6 className="font-heading font-semibold">Redes Sociais</h6>
-                  <div className="flex *:p-3 *:bg-gray-800 *:rounded gap-3 mt-4">
+                  <div className="flex *:p-3 *:bg-gray-800 *:rounded gap-3 mt-4 justify-center lg:justify-normal">
                      <Link href="https://www.facebook.com/profile.php?id=61563681704143" target="_blank">
                         <Facebook />
                      </Link>
@@ -55,10 +55,9 @@ const Footer = () => {
                </div>
             </div>
             {/* Políticas */}
-            <div className="flex justify-center gap-15 font-heading my-10">
+            <div className="flex justify-center flex-wrap md:flex-nowrap gap-7 md:gap-15 font-heading my-10 *:underline lg:*:no-underline">
                <Link href="">Aviso Legal</Link>
                <Link href="">Política de Cookies</Link>
-               <Link href="">Políticas de Cancelamento</Link>
                <Link href="">Políticas de Privacidade</Link>
                <Link href="">Termos de Uso</Link>
             </div>
