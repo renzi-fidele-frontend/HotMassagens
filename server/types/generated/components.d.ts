3d2479bf6855d5a76340eea9b8a7a9a5d00e44a3
Renzi Fidele <renzifidele2001@gmail.com>
2025-06-08 15:34:12 +0200

    fix: Tentando corrigir os erros de build

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,19 +1,22 @@
-import type { Attribute, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface LocalLocalDeAtendimento extends Schema.Component {
+export interface LocalLocalDeAtendimento extends Struct.ComponentSchema {
   collectionName: 'components_local_local_de_atendimentos';
   info: {
     displayName: 'local de atendimento';
     icon: 'pinMap';
   };
   attributes: {
-    descricao: Attribute.RichText;
-    fotos: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-    horario: Attribute.RichText;
+    descricao: Schema.Attribute.RichText;
+    fotos: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    horario: Schema.Attribute.RichText;
   };
 }
 
-export interface SiteCaracteristicas extends Schema.Component {
+export interface SiteCaracteristicas extends Struct.ComponentSchema {
   collectionName: 'components_site_caracteristicas';
   info: {
     description: '';
@@ -21,45 +24,45 @@ export interface SiteCaracteristicas extends Schema.Component {
     icon: 'emotionHappy';
   };
   attributes: {
-    altura: Attribute.Decimal;
-    cabelo: Attribute.String;
-    fumante: Attribute.Boolean;
-    gluteos: Attribute.String;
-    idade: Attribute.Integer;
-    lingerie: Attribute.String;
-    nacionalidade: Attribute.String;
-    olhos: Attribute.String;
-    perfume: Attribute.String;
-    peso: Attribute.Decimal;
-    piercings: Attribute.Boolean;
-    tatuagens: Attribute.String;
+    altura: Schema.Attribute.Decimal;
+    cabelo: Schema.Attribute.String;
+    fumante: Schema.Attribute.Boolean;
+    gluteos: Schema.Attribute.String;
+    idade: Schema.Attribute.Integer;
+    lingerie: Schema.Attribute.String;
+    nacionalidade: Schema.Attribute.String;
+    olhos: Schema.Attribute.String;
+    perfume: Schema.Attribute.String;
+    peso: Schema.Attribute.Decimal;
+    piercings: Schema.Attribute.Boolean;
+    tatuagens: Schema.Attribute.String;
   };
 }
 
-export interface SitePersonalidade extends Schema.Component {
+export interface SitePersonalidade extends Struct.ComponentSchema {
   collectionName: 'components_site_personalidades';
   info: {
     displayName: 'personalidade';
     icon: 'plane';
   };
   attributes: {
-    nome: Attribute.String;
+    nome: Schema.Attribute.String;
   };
 }
 
-export interface SitePreco extends Schema.Component {
+export interface SitePreco extends Struct.ComponentSchema {
   collectionName: 'components_site_precos';
   info: {
     displayName: 'preco';
     icon: 'shoppingCart';
   };
   attributes: {
-    duracao: Attribute.String;
-    preco: Attribute.Integer;
+    duracao: Schema.Attribute.String;
+    preco: Schema.Attribute.Integer;
   };
 }
 
-export interface SiteServico extends Schema.Component {
+export interface SiteServico extends Struct.ComponentSchema {
   collectionName: 'components_site_servicos';
   info: {
     description: '';
@@ -67,15 +70,15 @@ export interface SiteServico extends Schema.Component {
     icon: 'dashboard';
   };
   attributes: {
-    icone: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    nome: Attribute.String;
-    precos: Attribute.Component<'site.preco', true>;
+    icone: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    nome: Schema.Attribute.String;
+    precos: Schema.Attribute.Component<'site.preco', true>;
   };
 }
 
-declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
       'local.local-de-atendimento': LocalLocalDeAtendimento;
       'site.caracteristicas': SiteCaracteristicas;
       'site.personalidade': SitePersonalidade;
