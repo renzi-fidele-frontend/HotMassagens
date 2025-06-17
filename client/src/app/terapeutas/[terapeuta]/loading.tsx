1daae35b19926b53bf2c31b5880aab2bec523e55
Renzi Fidele <renzifidele2001@gmail.com>
2025-06-17 20:50:00 +0200

    feat: Adicionando o loading ao se apanhar os detalhes da terapeuta

---
+++ client/src/app/terapeutas/[terapeuta]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+   return (
+      <div className="bg-linear-to-b from-black to-gray-800 text-gray-200 text-center min-h-[60dvh] flex items-center justify-center text-xl">
+         <p>Carregando o perfil da terapeuta...</p>
+      </div>
+   );
+}
