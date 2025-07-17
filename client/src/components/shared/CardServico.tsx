dde1b4aa420198ef9bcd52f55645558eedde58d9
Renzi Fidele <renzifidele2001@gmail.com>
2025-07-17 13:36:48 +0200

    feat: Implementando tooltip para desktop e colapsável para mobile na descrição do serviço prestado pela terapeuta

--- client/src/components/shared/CardServico.tsx
+++ client/src/components/shared/CardServico.tsx
@@ -2,6 +2,8 @@ import { Iterapeuta } from "@/types/global";
 import Image from "next/image";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { Info } from "lucide-react";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible";
+import Btn from "./Btn";
 
 const CardServico = ({ servico }: { servico: Iterapeuta["servicos"][0] }) => {
    return (
@@ -25,22 +27,33 @@ const CardServico = ({ servico }: { servico: Iterapeuta["servicos"][0] }) => {
                <p>Valor à Combinar.</p>
             )}
          </div>
-         {/* Tooltip do desktop */}
+
          {servico.descricao && (
-            <div className="absolute right-4 top-4 hidden lg:block">
-               <Tooltip>
-                  <TooltipTrigger>
-                     <Info className="hover:cursor-pointer" />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                     <div className="max-w-[240px] md:max-w-sm">
-                        <p>{servico?.descricao}</p>
-                     </div>
-                  </TooltipContent>
-               </Tooltip>
-            </div>
+            <>
+               {/* Tooltip do desktop */}
+               <div className="absolute right-4 top-4 hidden lg:block">
+                  <Tooltip>
+                     <TooltipTrigger>
+                        <Info className="hover:cursor-pointer" />
+                     </TooltipTrigger>
+                     <TooltipContent>
+                        <div className="max-w-[240px] md:max-w-sm">
+                           <p>{servico?.descricao}</p>
+                        </div>
+                     </TooltipContent>
+                  </Tooltip>
+               </div>
+               {/* Collapsible do Mobile */}
+               <Collapsible className="lg:hidden">
+                  <CollapsibleTrigger>
+                     <Btn>
+                        <Info /> Ver detalhes
+                     </Btn>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent className="mt-5">{servico?.descricao}</CollapsibleContent>
+               </Collapsible>
+            </>
          )}
-         {/*  */}
       </div>
    );
 };
