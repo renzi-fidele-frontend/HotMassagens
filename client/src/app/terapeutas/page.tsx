0497f28f5a5cf6b3c49cbe7e70268a691e1c3ed2
Renzi Fidele <renzifidele2001@gmail.com>
2025-07-23 12:11:08 +0200

    feat: Adicionando a estrutura básica do cabeçalho da página de todas as terapeutas

--- client/src/app/terapeutas/page.tsx
+++ client/src/app/terapeutas/page.tsx
@@ -1,3 +1,38 @@
+import Container from "@/components/layout/Container";
+import { HeadingXl } from "@/components/shared/Typography";
+import { Input } from "@/components/ui/input";
+import { Search, Venus } from "lucide-react";
+
 export default function Terapeutas() {
-   return <div></div>;
+   return (
+      <div className="bg-linear-to-b from-black to-gray-800 pb-15 lg:pb-20 text-white">
+         <Container>
+            {/* Heading */}
+            <div className="flex items-end gap-6 pt-8 pb-5">
+               <HeadingXl>Nossas terapeutas</HeadingXl>
+               <div className="flex border rounded-4xl border-zinc-600 py-2 px-7 gap-2.5 font-semibold">
+                  <Venus /> 8 Disponíveis
+               </div>
+            </div>
+            {/* Separador */}
+            <div className="mask-r-from-0%">
+               <hr className="bg-gradiente py-1 border-0" />
+            </div>
+            {/* Pesquisa */}
+            <div className="relative flex items-center mt-10 mb-9 ">
+               <Input placeholder="Buscar por nome..." className="ps-13 placeholder:text-lg py-6 border-zinc-500" />
+               <Search className="absolute start-4 text-zinc-400" />
+            </div>
+            {/* Corpo */}
+            <main className="flex gap-8">
+                <div className="basis-[25%]">
+                    <div>
+                        
+                    </div>
+                </div>
+                <div></div>
+            </main>
+         </Container>
+      </div>
+   );
 }
