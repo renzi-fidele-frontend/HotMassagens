fd5baed421fa1bbae18a285e1b19fe2c04543bec
Renzi Fidele <renzifidele2001@gmail.com>
2025-08-14 18:35:17 +0200

    feat: Adicionando ícone de Home ao botão de retorno na página de 404

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -4,7 +4,9 @@ import foto from "@/../public/images/404Optz.webp";
 import { HeadingXl } from "@/components/shared/Typography";
 import Btn from "@/components/shared/Btn";
 import Link from "next/link";
+import { Home } from "lucide-react";
 
+// TODO: A página de not-found não é rendereizada quando visito uma rota inexistente
 const notFound = () => {
    return (
       <div className="bg-linear-to-b from-black to-gray-800 pt-10 pb-20">
@@ -13,7 +15,9 @@ const notFound = () => {
             <HeadingXl>Página não encontrada</HeadingXl>
             <p className="text-lg sm:text-2xl">A página que você procurou não está disponível</p>
             <Link href="/">
-               <Btn>Regressar ao início</Btn>
+               <Btn>
+                  <Home /> Regressar ao início
+               </Btn>
             </Link>
          </Container>
       </div>
