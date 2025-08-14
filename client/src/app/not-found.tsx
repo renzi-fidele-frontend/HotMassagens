31abe27eb998d87631c822a76e6f4925b5cd128b
Renzi Fidele <renzifidele2001@gmail.com>
2025-08-14 14:47:03 +0200

    feat: Melhorando a estrutura da página de Not Found

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,6 +1,23 @@
+import Container from "@/components/layout/Container";
+import Image from "next/image";
+import foto from "@/../public/images/404Optz.webp";
+import { HeadingXl } from "@/components/shared/Typography";
+import Btn from "@/components/shared/Btn";
+import Link from "next/link";
+
 const notFound = () => {
-   // TODO: Adicionar UI da página de NotFound(404)
-   return <div>notFound</div>;
+   return (
+      <div className="bg-linear-to-b from-black to-gray-800 pt-10 pb-20">
+         <Container className="flex flex-col gap-11 items-center text-center">
+            <Image className="w-[80%] sm:w-auto" src={foto} alt="Ilustração de uma boneca dando massagem a um cliente" />
+            <HeadingXl>Página não encontrada</HeadingXl>
+            <p className="text-lg sm:text-2xl">A página que você procurou não está disponível</p>
+            <Link href="/">
+               <Btn>Regressar ao início</Btn>
+            </Link>
+         </Container>
+      </div>
+   );
 };
 
 export default notFound;
